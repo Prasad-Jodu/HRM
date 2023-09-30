b39e3f45a947aa22776acdaf16e0b95bbfd04a28
Prasad-Jodu <prasadjodu12@gmail.com>
2023-09-30 15:50:38 +0530

    Ok commit

---
+++ src/test/java/jenkinsPractice/TrailTest.java
@@ -0,0 +1,11 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class TrailTest {
+	@Test
+	public void trailTest() {
+		System.out.println("This is the first trail");
+	}
+
+}
